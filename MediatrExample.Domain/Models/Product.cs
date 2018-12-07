383e984b1dddb1b08eda67935081fdcf5e289917
Milton Câmara <miltomcamara@gmail.com>
2018-12-07 16:47:11 -0200

    Added category property

--- MediatrExample.Domain/Models/Product.cs
+++ MediatrExample.Domain/Models/Product.cs
@@ -7,6 +7,7 @@ namespace MediatrExample.Domain.Models
     {
         public string Name { get; set; }
         public decimal Price { get; set; }
+        public string Category { get; set; }
 
         public Product(string name, decimal price)
         {
